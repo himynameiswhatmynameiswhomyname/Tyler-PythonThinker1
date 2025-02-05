89b42f33a0af7555889d064eef203acfe75f9170
himynameiswhatmynameiswhomyname <tylerjkleinman@gmail.com>
2025-02-05 08:28:18 +0000

    Feb 5, 2025, 8:28 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -6,19 +6,19 @@
 # calculate the total amount you have spent. Different coloured
 # plates costs different as shown below:
 
-Red = 1
-Blue = 2
-Green = 3
+# Red = 1
+# Blue = 2
+# Green = 3
 
 # You have ordered a total of 3 red plates, 5 blue plates, and 4
 # green plates.
 
 # Calculate and print the total amount you have spent: -->
-r_amount = Red*3
-b_amount = Blue*5
-g_amount = Green*4
-ans = r_amount + b_amount + g_amount
-print(ans)
+# r_amount = Red*3
+# b_amount = Blue*5
+# g_amount = Green*4
+# ans = r_amount + b_amount + g_amount
+# print(ans)
 ## Task 1: Storing and printing Strings
 
 # **Task 1a**:

3c2743dbdb7515e519179bb600c9c288a3675c61
himynameiswhatmynameiswhomyname <tylerjkleinman@gmail.com>
2025-02-05 08:19:37 +0000

    Feb 5, 2025, 8:19 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -46,7 +46,7 @@ print(age)
 # Concatenate the name with "Hi, [name]!" and print the
 # complete message.
 print = input("what is your name: ")
-print
+print ("")
 # **Task 2b**:
 # Use input() to ask the user for their favorite hobby. Store this
 # in a variable.

a9dfcc002e5d46cd8860911e301ee73d6b22b332
himynameiswhatmynameiswhomyname <tylerjkleinman@gmail.com>
2025-02-05 08:11:42 +0000

    Feb 5, 2025, 8:11 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -24,7 +24,7 @@ print(ans)
 # **Task 1a**:
 # Use the input() function to ask the user for their name and
 # store it in a variable. Then print that variable.
-input("enter your name:Tyler")
+username = input("enter your name:Tyler")
 
 # **Task 1b**:
 # Ask the user for their favorite colour using input() and

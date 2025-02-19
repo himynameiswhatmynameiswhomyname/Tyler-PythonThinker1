33cd2d5d791c4ea9d8cfc66d558a76f9303ca4a9
himynameiswhatmynameiswhomyname <tylerjkleinman@gmail.com>
2025-02-19 08:44:34 +0000

    Feb 19, 2025, 8:44 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -21,21 +21,21 @@
 # Fix the errors in the following:
 
 # **Task 1a**:
-# for i in range(3)
-#     print("Hello, World!")
+for i in range(3):
+    print("Hello, World!")
 
 # **Task 1b**:
-# for i in range(5):
-# print(i)
+for i in range(5):
+    print(i)
 
 # **Task 1c**:
-# print("Hello, World!)
+print("Hello, World!")
 
 # **Task 1d**:
-# if = 5
+if - 5:
 
 # **Task 1e**:
-# print "Hello, World!"
+ print("Hello, World!")
 
 
 

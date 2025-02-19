5885158315f140989453be234ff556ef83a6a13f
himynameiswhatmynameiswhomyname <tylerjkleinman@gmail.com>
2025-02-19 08:40:06 +0000

    Feb 19, 2025, 8:40 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -10,14 +10,32 @@
 # each student one at a time.
 
 # Use only variables, math operators that you have learnt, as# well as a 'for' loop
-numStudents = int(input("how many students are there "))
-sum = 0
-for i in range(1,numStudents+1):
-    sum=sum+int(input("what is student #" + "'s score"))
-print("My sum is: " + str(sum)) 
-print("my average is; " +str(sum/ numStudents))
+# numStudents = int(input("how many students are there "))
+# sum = 0
+# for i in range(1,numStudents+1):
+#     sum=sum+int(input("what is student #" + "'s score"))
+# print("My sum is: " + str(sum)) 
+# print("my average is; " +str(sum/ numStudents))
 
+## Task 1: Syntax Errors
+# Fix the errors in the following:
 
+# **Task 1a**:
+# for i in range(3)
+#     print("Hello, World!")
+
+# **Task 1b**:
+# for i in range(5):
+# print(i)
+
+# **Task 1c**:
+# print("Hello, World!)
+
+# **Task 1d**:
+# if = 5
+
+# **Task 1e**:
+# print "Hello, World!"
 
 
 

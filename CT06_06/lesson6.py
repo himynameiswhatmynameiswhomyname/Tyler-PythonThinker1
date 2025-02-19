abd483a821dddf224ebc4f50d8c6b9743466b3cc
himynameiswhatmynameiswhomyname <tylerjkleinman@gmail.com>
2025-02-19 08:12:16 +0000

    Feb 19, 2025, 8:12 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -17,4 +17,4 @@ import webbrowser
 # Open Roblox website in the default web browser
 webbrowser.open('https://www.roblox.com')
 
-numofstudent = int(input())
+numofstudent = int(input("how many "))
